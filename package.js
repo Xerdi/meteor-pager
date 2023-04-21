2608e1503452923f73f4d9d887aaa25db3c0021b
Erik Nijenhuis <erik@xerdi.com>
2023-04-21 13:51:39 +0200

    Update versions

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
     name: 'xerdi:pager',
-    version: '0.0.2',
+    version: '0.0.3',
     summary: 'Bootstrap pager',
     git: 'https://github.com/Xerdi/meteor-pager.git',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('2.8.0');
+    api.versionsFrom('2.11.0');
     api.use([
         'ecmascript',
         'reactive-var',
-        'blaze-html-templates@1.2.1',
-        'xerdi:common-helpers@0.0.1'
+        'blaze-html-templates@2.0.0',
+        'xerdi:common-helpers@0.0.3'
     ]);
     api.mainModule('index.js', 'client');
 });
